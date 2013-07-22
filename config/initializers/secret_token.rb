16c28cfb0ece51b00d11892f80cd50c2db7e8258
Sergio Díaz <sendorf@gmail.com>
2013-07-22 11:37:31 +0200

    Creada la aplicación rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Conan::Application.config.secret_token = 'e81a70cc3e628f8d779bbb5a12c0207f0d01b6b27c4e18798d6e3bb8b72eb303e2334e63f612e463ed2535f3aaed473d626defa58e425a1b0f57e3c74207910e'
