94c2dfb4680df756341fe3f4d3b4e878f95d0f32
Sendorf <sendorf@gmail.com>
2013-07-29 12:45:02 +0200

    Modificado el gitignore e intento de hacer correr la página

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Load heroku vars from local file
+alwaysdata_env = File.join(Rails.root, 'config', 'alwaysdata_env.rb')
+load(alwaysdata_env) if File.exists?(alwaysdata_env)
+
 # Initialize the rails application
 Conan::Application.initialize!
