a6b86ed3bc07aa8ab9fbcce424c7c1c96971d237
Sergio Díaz <sendorf@gmail.com>
2013-07-29 12:25:45 +0200

    Ya sale algo en el index

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,4 +1,4 @@
-Comandes::Application.configure do
+Conan::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
