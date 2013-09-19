1c276a72500b7d4ae74d2122950da16c62dbdb87
Sergio Díaz <sendorf@gmail.com>
2013-09-19 16:24:31 +0200

    Funciona javascript

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -18,10 +18,10 @@ Conad::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  #config.assets.compile = true
 
   # Generate digests for assets URLs
-  config.assets.digest = true
+  #config.assets.digest = true
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
