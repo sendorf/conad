2b401afe2db455092c8e1eda109ebc018d49ae55
Sergio Díaz <sendorf@gmail.com>
2013-11-15 13:40:35 +0100

    Ya funcionan los links del calendario

--- app/assets/javascripts/conad.js
+++ app/assets/javascripts/conad.js
@@ -1,7 +1,16 @@
 $(function(){
 
+  $('#calendar').on('click', "[data-link]", function(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    alert("I am an alert box!");
+    var href = $(this).data('link');
+    window.location.href = href;
+  });
+
   // Hide alerts after a while
   $(".alert").delay(500).fadeIn().delay(4000).fadeOut();
 
-
 });
+
+
