7412d3634bcad0da064bac3406fde5d4717d874d
sendorf <sendorf@gmail.com>
2014-07-15 18:43:30 +0200

    minor changes in the chart style

--- app/assets/javascripts/conad.js
+++ app/assets/javascripts/conad.js
@@ -56,8 +56,14 @@ $(function(){
       .call(xAxis);
 
   chart.append("g")
-      .attr("class", "y axis")
-      .call(yAxis);
+    .attr("class", "y axis")
+    .call(yAxis)
+  .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text("Number");
 
   chart.selectAll(".bar")
       .data(data)
