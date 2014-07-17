7e074ca77715b86f9a514e1e031f99c4ac41cb4a
sendorf <sendorf@gmail.com>
2014-07-17 11:17:43 +0200

    Created a line chart representing the connections to the different servers in the application

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -3,6 +3,8 @@ class ChartsController < ApplicationController
 	def index
 		@servers = Server.all.sort
     @server = Server.find(1)
+    @end_date = DateTime.now
+    @start_date = @end_date -30.days
 	end
 	
 end
--- app/models/connection.rb
+++ app/models/connection.rb
@@ -13,7 +13,41 @@ class Connection < ActiveRecord::Base
   scope :month_server_connections,  lambda{|month, server| select('start_time, server_id')
                                                           .where(["(server_id = ? AND extract(month from start_time) = ?)", server.id, month])
                                                           .order("start_time ASC")}
+  scope :period_connections, lambda{|start_date, end_date| where(["(start_time >= ? AND start_time <= ?)", start_date, end_date])
+                                                          .order("start_time ASC")}
 
+
+  def self.chart_format_period_connections(start_date, end_date)
+    date = start_date.to_date
+    servers = Server.all.to_a
+    i = 0
+    count_array = Array.new(servers.length){|i| i = 0}
+    result = []
+    period_connections(start_date, end_date).each do |connection|
+      found = false
+      while !found do
+        server = servers[i]
+        if connection.start_time.to_date == date && connection.server_id == server.id
+          count_array[i] += 1
+          found = true
+        elsif connection.start_time.to_date != date && connection.server_id == server.id
+          date_hash = {date: date.strftime('%Y%m%d')}
+          j = 0
+          servers.each do |serve|
+            date_hash["#{serve.name}"] = count_array[j]
+            j += 1
+          end
+          result << date_hash
+          date = connection.start_time.to_date
+          count_array = Array.new(servers.length){|i| i = 0}
+          found = true
+        end
+        i += 1
+      end
+      i = 0
+    end
+    return result
+  end
 
   def self.month_and_server_chart_data(month, server)
     day = 1
