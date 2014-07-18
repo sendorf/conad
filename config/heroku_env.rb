c7c34ea9f57fa8cf39f06c89a22426bb9dffd891
sendorf <sendorf@gmail.com>
2014-07-18 19:37:03 +0200

    Updated to upload the web to heroku

--- config/heroku_env.rb
+++
@@ -1,3 +0,0 @@
-ENV["DATABASE_URL"] = "postgres://ecgqtkgprobzbd:n-FIF6oAFDXNBBI5bXtZmYTcx9@ec2-79-125-21-60.eu-west-1.compute.amazonaws.com:5432/dch64u0raja5q2"
-
-ENV["EXECJS_RUNTIME"] = "RubyRacer"
