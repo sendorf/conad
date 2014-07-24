c5d997e42a83170f5c941c79ee352739fbac6a45
sendorf <sendorf@gmail.com>
2014-07-24 13:59:29 +0200

    Created a Stacked bar chart. Resolves #12

--- app/assets/javascripts/conad.js
+++ app/assets/javascripts/conad.js
@@ -70,8 +70,108 @@ $(function(){
     }
 });
 
+$(function(){
 
+  var data = $('.stacked-bar-chart').data('chart')
 
+  if(data){
+
+    var margin = {top: 20, right: 20, bottom: 30, left: 40},
+                 width = 960 - margin.left - margin.right,
+                 height = 500 - margin.top - margin.bottom;
+
+    var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .1);
+
+    var y = d3.scale.linear()
+        .rangeRound([height, 0]);
+
+    var parseDate = d3.time.format("%Y%m%d").parse;
+
+    var color = d3.scale.category20b();
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .tickFormat(d3.format(".2s"));
+
+    var svg = d3.select('.stacked-bar-chart')
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
+
+    data.forEach(function(d) {
+        d.date = parseDate(d.date.toString());
+    });
+
+    data.forEach(function(d) {
+      var y0 = 0;
+      d.servers = color.domain().map(function(name) { return {name: name, y0: y0, y1: y0 += +d[name]}; });
+      d.total = d.servers[d.servers.length - 1].y1;
+    });
+
+    data.sort(function(a, b) { return b.total - a.total; });
+
+    x.domain(data.map(function(d) { return d.date; }));
+    y.domain([0, d3.max(data, function(d) { return d.total; })]);
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+      .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Connections");
+
+    var state = svg.selectAll(".date")
+        .data(data)
+      .enter().append("g")
+        .attr("class", "g")
+        .attr("transform", function(d) { return "translate(" + x(d.date) + ",0)"; });
+
+    state.selectAll("rect")
+        .data(function(d) { return d.servers; })
+      .enter().append("rect")
+        .attr("width", x.rangeBand())
+        .attr("y", function(d) { return y(d.y1); })
+        .attr("height", function(d) { return y(d.y0) - y(d.y1); })
+        .style("fill", function(d) { return color(d.name); });
+
+    var legend = svg.selectAll(".legend")
+        .data(color.domain().slice().reverse())
+      .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+    legend.append("rect")
+        .attr("x", width - 18)
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", color);
+
+    legend.append("text")
+        .attr("x", width - 24)
+        .attr("y", 9)
+        .attr("dy", ".35em")
+        .style("text-anchor", "end")
+        .text(function(d) { return d; });
+
+  }
+});
 
 $(function(){
 
@@ -92,7 +192,7 @@ $(function(){
     var y = d3.scale.linear()
         .range([height, 0]);
 
-    var color = d3.scale.category10();
+    var color = d3.scale.category20b();
 
     var xAxis = d3.svg.axis()
         .scale(x)
@@ -105,7 +205,7 @@ $(function(){
     var line = d3.svg.line()
         .interpolate("basis")
         .x(function(d) { return x(d.date); })
-        .y(function(d) { return y(d.temperature); });
+        .y(function(d) { return y(d.connections); });
 
     var chart = d3.select(".lines-chart")
         .attr("width", width + margin.left + margin.right)
@@ -119,11 +219,11 @@ $(function(){
         d.date = parseDate(d.date.toString());
       });
 
-      var cities = color.domain().map(function(name) {
+      var servers = color.domain().map(function(name) {
         return {
           name: name,
           values: data.map(function(d) {
-            return {date: d.date, temperature: +d[name]};
+            return {date: d.date, connections: +d[name]};
           })
         };
       });
@@ -131,8 +231,8 @@ $(function(){
       x.domain(d3.extent(data, function(d) { return d.date; }));
 
       y.domain([
-        d3.min(cities, function(c) { return d3.min(c.values, function(v) { return v.temperature; }); }),
-        d3.max(cities, function(c) { return d3.max(c.values, function(v) { return v.temperature; }); })
+        d3.min(servers, function(c) { return d3.min(c.values, function(v) { return v.connections; }); }),
+        d3.max(servers, function(c) { return d3.max(c.values, function(v) { return v.connections; }); })
       ]);
 
       chart.append("g")
@@ -150,19 +250,19 @@ $(function(){
           .style("text-anchor", "end")
           .text("Connections");
 
-      var city = chart.selectAll(".city")
-          .data(cities)
+      var server = chart.selectAll(".server")
+          .data(servers)
         .enter().append("g")
-          .attr("class", "city");
+          .attr("class", "server");
 
-      city.append("path")
+      server.append("path")
           .attr("class", "line")
           .attr("d", function(d) { return line(d.values); })
           .style("stroke", function(d) { return color(d.name); });
 
-      city.append("text")
+      server.append("text")
           .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
-          .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")"; })
+          .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.connections) + ")"; })
           .attr("x", 3)
           .attr("dy", ".35em")
           .text(function(d) { return d.name; });
