da7c43b4671a426c88d40be323c9c56c2c90df14
sendorf <sendorf@gmail.com>
2014-08-19 11:08:20 +0200

    Reset Heroku DB

--- config/heroku_env.rb
+++ config/heroku_env.rb
@@ -1,3 +1,3 @@
-ENV["DATABASE_URL"] = "postgres://punxfroblufzap:M7jHCKnnHOr9_oKw3tXnHQl7mm@ec2-54-228-227-87.eu-west-1.compute.amazonaws.com:5432/d2rpq1ko65oahj"
+ENV["DATABASE_URL"] = "postgres://foacejdrvsnnpg:VXdra-G3Q-DukWWn_tcoJbmows@ec2-54-246-103-31.eu-west-1.compute.amazonaws.com:5432/dldl2kmbeg677"
 
 ENV["EXECJS_RUNTIME"] = "RubyRacer"
