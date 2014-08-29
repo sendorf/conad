33cfff6855139feaf6b90d9b47bed300b03b09cc
Sendorf <Sendorf@gmail.com>
2014-08-29 21:14:38 +0200

    Updated DB

--- config/heroku_env.rb
+++ config/heroku_env.rb
@@ -1,3 +1,3 @@
-ENV["DATABASE_URL"] = "postgres://punxfroblufzap:M7jHCKnnHOr9_oKw3tXnHQl7mm@ec2-54-228-227-87.eu-west-1.compute.amazonaws.com:5432/d2rpq1ko65oahj"
+ENV["DATABASE_URL"] = "postgres://oiqstsizbtsppn:S-0y7HvcQ2F8JMvMLsDRrVL58a@ec2-54-246-88-248.eu-west-1.compute.amazonaws.com:5432/daau46gou7kjam"
 
 ENV["EXECJS_RUNTIME"] = "RubyRacer"
