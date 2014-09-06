da4a82f1ba88dc62f53839a471ebac89c1857467
sendorf <sendorf@gmail.com>
2014-09-06 11:47:25 +0200

    Changed the DB in Heroku

--- config/heroku_env.rb
+++ config/heroku_env.rb
@@ -1,3 +1,3 @@
-ENV["DATABASE_URL"] = "postgres://foacejdrvsnnpg:VXdra-G3Q-DukWWn_tcoJbmows@ec2-54-246-103-31.eu-west-1.compute.amazonaws.com:5432/dldl2kmbeg677"
+ENV["DATABASE_URL"] = "postgres://qohgosllfsyvat:9I9QHFqd3qg0n6vDvB6GAOfAWk@ec2-54-217-211-247.eu-west-1.compute.amazonaws.com:5432/d7sksmsp8rblda"
 
 ENV["EXECJS_RUNTIME"] = "RubyRacer"
