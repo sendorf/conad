8cd47d8a6c42a77f704303a294c75fe6c0dc3207
sendorf <sendorf@gmail.com>
2014-09-17 22:41:02 +0200

    Unified the top buttons in a partial to render

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -2,7 +2,7 @@ class DaysController < ApplicationController
 
   def show
     @servers = Server.all.sort
-    @date = DateTime.parse(params[:day],"%Y-%m-%d")
+    @day = DateTime.parse(params[:day],"%Y-%m-%d")
     if params[:server_id] 
       @server = Server.find(params[:server_id]) 
     end
