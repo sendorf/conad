ad994c79099d3fac3ce5e0837e8efdd7eea08f70
Sendorf <sendorf@gmail.com>
2015-05-20 00:10:12 +0200

    Changed the db to the local one

--- config/environment.rb
+++ config/environment.rb
@@ -2,8 +2,8 @@
 require File.expand_path('../application', __FILE__)
 
 # Load alwaysdata vars from local file
-heroku_env = File.join(Rails.root, 'config', 'heroku_env.rb')
-load(heroku_env) if File.exists?(heroku_env)
+# heroku_env = File.join(Rails.root, 'config', 'heroku_env.rb')
+# load(heroku_env) if File.exists?(heroku_env)
 
 # Initialize the Rails application.
 Rails.application.initialize!
